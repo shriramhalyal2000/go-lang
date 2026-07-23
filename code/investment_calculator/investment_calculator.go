d9d80e16fb0c246af62758bdc3b8ee7d17be71c6
shriramhalyal2000 <shriramhalyal48@gmail.com>
2026-07-23 19:33:08 +0530

    added inflationAdjustedRate func in code

--- code/investment_calculator/investment_calculator.go
+++ code/investment_calculator/investment_calculator.go
@@ -5,10 +5,23 @@ import (
 	"math"
 )
 
+func adjustedInflationRate() {
+	const inflationRate = 2.5
+	var investmentAmount float64
+	fmt.Println("Enter your invest ment under billion$:")
+	fmt.Scanln(&investmentAmount)
+	rateOfReturn := 5.5
+	years := 10.0
+	returnAmount := investmentAmount * math.Pow(1+rateOfReturn/100, years)
+	inflationAdjustedAmt := returnAmount * math.Pow(1+inflationRate/100, years)
+	fmt.Println("The investment amount adjusted to inflation is :", inflationAdjustedAmt)
+
+}
+
 func mini_invest() {
 	invest, rate, time := 10000.0, 5.5, 10.0 // decalre multiple variables without explicit type or var def
 	short_term_val := invest * math.Pow(1+rate/100, time)
-	fmt.Println(short_term_val)
+	fmt.Println("short term return rate", short_term_val)
 
 }
 
@@ -22,13 +35,12 @@ func investment() {
 	fmt.Scanln(&investment_amt)
 
 	future_val = investment_amt * math.Pow(1+return_rate/100, float64(time))
-	fmt.Println(future_val)
+	fmt.Println("future adjusted value is ", future_val)
 
 }
 
 func main() {
-	fmt.Print("The investment fund is : \n")
 	investment()
-	fmt.Println("Short term investment return:")
 	mini_invest()
+	adjustedInflationRate()
 }
