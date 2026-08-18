8698aae626f35821d086fa67df1f2c664a97da90
shriramhalyal2000 <shriramhalyal48@gmail.com>
2026-08-18 13:22:30 +0530

    updated with fileexist function

--- fileHandling/read/main.go
+++ fileHandling/read/main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 )
 
 func main() {
+	file, fault := FileExists("readme")
+	if fault != nil {
+		return
+	}
+	fmt.Println(file)
 	content, err := ReadFile("readme")
 	if err != nil {
 		return
 	}
 	fmt.Println(content)
 }
+
+// takes in file path as string, and returns contents and an potential error
+
 func ReadFile(file string) (string, error) {
 	data, err := os.ReadFile(file)
 	if err != nil {
@@ -21,4 +30,20 @@ func ReadFile(file string) (string, error) {
 	fmt.Println("The file data is:")
 	return string(data), nil
 
+}
+
+// takes in file as an argument and returns file exists or not as bool and a potential error
+
+func FileExists(path string) (bool, error) {
+	_, err := os.Lstat(path)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			fmt.Println("file not found!")
+			return false, nil
+		}
+		fmt.Println("Error!, file not found.")
+		return false, err
+	}
+	fmt.Println("File exists!")
+	return true, nil
 }
